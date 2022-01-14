97ac758dcc2e3a03e991b24e570d937c3c165c20
lani <laninq04@gmail.com>
2022-01-13 20:02:34 -0500

    ✨ Added controls for intake

--- src/main/cpp/controller/ControlData.cpp
+++ src/main/cpp/controller/ControlData.cpp
@@ -43,12 +43,14 @@ void Controller::updateControlData(const ControllerData &controllerData, Control
         controlData.rDrive = controllerData.pRYStick;
     }
 
-    //Intake:
-    controlData.mIntakeDown = controllerData.sRBumper;
-    controlData.mIntakeRollers = ((controllerData.sRTrigger > 0.5) && !controlData.shift);
-    controlData.mIntakeRollersBackward = ((controllerData.sRTrigger > 0.5) && controlData.shift);
-    controlData.saIntake = (controllerData.sRTrigger > 0.5);
-    controlData.saIntakeBackward = ((controllerData.sLTrigger > 0.5) && !controlData.shift);
+
+    //INTAKE::
+    controlData.mIntakeDown = controllerData.sYBtn;
+    controlData.mIntakeRollers = controllerData.sRBumper;
+    controlData.mIntakeRollersBackward = controllerData.sABtn;
+    controlData.saIntake = controllerData.sRBumper;
+    controlData.saIntakeBackward = controllerData.sABtn;
+
 
     
 }
