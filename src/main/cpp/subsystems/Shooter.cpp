ab46bc57f6fb1a851e268f304160923d3937134f
Tananya <tprankprakma@gmail.com>
2022-02-22 18:50:24 -0500

    🔨 Changed readyShoot logic
    to enter readyShoot true, wheel velocity must be at least above 30 under desired velocity. however, to exit readyShoot true and enter readyShoot false, you must be 100 under.

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -115,12 +115,13 @@ void Shooter::semiAuto(const RobotData &robotData, ShooterData &shooterData){
         // if (getWheelVel() > (wheelSpeed - 50))
 
         //once it's a high enough velocity its ready for indexer to run
-        
-        if (getWheelVel() > (robotData.limelightData.desiredVel - 30))
+        if (shooterData.readyShoot == false && (getWheelVel() > (robotData.limelightData.desiredVel - 30)))
+        // if you're not in readyShoot yet and the wheel velocity is above 30 under the desire velocity, readyShoot will become true
         {
             shooterData.readyShoot = true;
         }
-        else
+        else if (shooterData.readyShoot == true && (getWheelVel() < (robotData.limelightData.desiredVel - 100)))
+        // if you're already in readyShoot, you'll only exit readyShoot if the wheel velocity drops below 100 below the desired velocity
         {
             shooterData.readyShoot = false;
         }
