b26bc999c9d41cad377336cab6fa97b214092eab
Hendrik <hnsorens@gmail.com>
2022-03-28 18:38:24 -0400

    ✨ Uncommented Arduino Code
    Now the arduino will work.

--- src/main/cpp/common/LEDs.cpp
+++ src/main/cpp/common/LEDs.cpp
@@ -7,38 +7,38 @@
 void LEDs::RobotPeriodic(const RobotData &robotData, LEDsData &ledData){
     // this makes the robot LEDs be different colors depending on the mode
     // writes the value of colorCode to device address 1 (the left arduino), which then color codes the LEDs based upon the value
-    // if (robotData.shooterData.readyShoot){
-    //     colorCode = 5; //ready to shoot
-    // } else if (robotData.controlData.mode == Mode::mode_teleop_manual){
-    //     colorCode = 4; //teleop manual mode
-    // } else if (robotData.controlData.mode == Mode::mode_climb_sa){
-    //     colorCode = 3; //climb semiauto mode
-    // } else if (robotData.controlData.mode == Mode::mode_climb_manual){
-    //     colorCode = 2; //climb manual mode
-    // } else if (robotData.controlData.mode == Mode::mode_teleop_sa){
-    //     colorCode = 1; //teleop semiauto mode
-    // }
+    if (robotData.shooterData.readyShoot){
+        colorCode = 5; //ready to shoot
+    } else if (robotData.controlData.mode == Mode::mode_teleop_manual){
+        colorCode = 4; //teleop manual mode
+    } else if (robotData.controlData.mode == Mode::mode_climb_sa){
+        colorCode = 3; //climb semiauto mode
+    } else if (robotData.controlData.mode == Mode::mode_climb_manual){
+        colorCode = 2; //climb manual mode
+    } else if (robotData.controlData.mode == Mode::mode_teleop_sa){
+        colorCode = 1; //teleop semiauto mode
+    }
 
-    // char value[1] = {(char)colorCode};
+    char value[1] = {(char)colorCode};
 
-    // if (lastColorCode != colorCode){
-    //     arduino.Write(value, 1);
-    // }
+    if (lastColorCode != colorCode){
+        arduino.Write(value, 1);
+    }
 
-    // lastColorCode = colorCode;
-
-
-    // frc::SmartDashboard::PutNumber("Bytes received", arduino.GetBytesReceived());
-
-    // frc::SmartDashboard::PutNumber("ColorCode for LEDs", colorCode);
+    lastColorCode = colorCode;
 
 
+    frc::SmartDashboard::PutNumber("Bytes received", arduino.GetBytesReceived());
 
-    // if (arduino.GetBytesReceived() >= 1)
-    // {
-    //     arduino.Read(colors,1);
-    //     arduino.Reset();
+    frc::SmartDashboard::PutNumber("ColorCode for LEDs", colorCode);
 
-    // }
-    // ledData.ColorData = (int)colors[0];
+
+
+    if (arduino.GetBytesReceived() >= 1)
+    {
+        arduino.Read(colors,1);
+        arduino.Reset();
+
+    }
+    ledData.ColorData = (int)colors[0];
 }
