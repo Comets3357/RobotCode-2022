32d28f1aa2aa9499f35eab54c2c83bdff9911d61
lani <laninq04@gmail.com>
2022-04-10 12:19:35 -0400

    ✨ Account for intake when shooting
    use stronger pid on flywheel when intake is running to get up to speed, also edited the set point shots and how they get up to speed

--- src/main/include/subsystems/Intake.h
+++ src/main/include/subsystems/Intake.h
@@ -19,6 +19,8 @@ struct IntakeData
 {
     bool intakeIdle;
     bool absEncoderInRange = true;
+    bool usingIntake = false;
+
 
     //bench test
     float intakeBenchTestSpeed = 0;
@@ -69,7 +71,6 @@ private:
     int idleCount = 0;
     //used to update rev encoder with abs encoder
     int tickCount = 0;
-    double pivotArbFF = 0;
 
     bool isZeroed_pivot = false;
 
