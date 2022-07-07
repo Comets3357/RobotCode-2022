8abcb819e6278adc6f511289b59b714f949e5f68
Brian Travis <bztravis88@gmail.com>
2022-07-07 19:04:18 -0400

    🔨 Update characterization

--- src/main/cpp/subsystems/Drivebase.cpp
+++ src/main/cpp/subsystems/Drivebase.cpp
@@ -63,12 +63,12 @@ void Drivebase::RobotInit()
     // dbR.Config_kD(0, 0);
 
     // Atlas 04.07.22 Final tread center drop but not fresh treads
-    dbL.Config_kF(0, 0.072601);
-    dbL.Config_kP(0, 0.10767);
+    dbL.Config_kF(0, 0.071797);
+    dbL.Config_kP(0, 0.10814);
     dbL.Config_kD(0, 0);
 
-    dbR.Config_kF(0, 0.072601);
-    dbR.Config_kP(0, 0.10767);
+    dbR.Config_kF(0, 0.071797);
+    dbR.Config_kP(0, 0.10814);
     dbR.Config_kD(0, 0);
 
 
